d12e148f0af0f2c5b80e62dbe44827b7ca49079c
Sebastian Grünwald <sebiscodes@gmail.com>
2023-12-07 12:40:46 +0100

    .lnk extension has been removed
    .lnk extension has been removed

--- For Autodesk Inventor/CleanupProjects.cmd
+++ For Autodesk Inventor/CleanupProjects.cmd
@@ -1,4 +1,4 @@
-REM A code to delete and recreate shortcuts to project files (ipj) in the inventor project folder
+REM A code to create shortcuts to project files (ipj) in the inventor project folder
 REM Just insert the path of a folder containing your projects and to your inventor project folder path and run this script as an administrator
 
 REM Written by Sebastian G., sebiscodes@gmail.com, 07. Dec. 2023, Winterthur
@@ -8,13 +8,11 @@ REM !!! IMPORTANT !!! START IT AS AN ADMINISTRATOR !!!
 
 @echo off
 set source_folder=*Path to the folder containing all .ipj files (project files)*
-set target_folder=*Path to the inventor project folder (to create the .lnk files (shortcuts))*
-
-del "%target_folder%\*.lnk"
+set target_folder=*Path to the inventor project folder (to create the shortcuts)*
 
 setlocal enabledelayedexpansion
 for /r %source_folder% %%i in (*.ipj) do (
-  echo Creating link: "!target_folder!\%%~ni.lnk"
-  mklink "!target_folder!\%%~ni.lnk" "%%i"
+  echo Creating link: "!target_folder!\%%~ni"
+  mklink "!target_folder!\%%~ni" "%%i"
 )
 endlocal
